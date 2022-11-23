889e2362ebf02f9d22080bb41cadd6fe9fe2bfcf
Markus Davis <business@markusdavis.com>
2022-11-23 08:18:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-      extend: {
-        colors: {
-          primary: "#1da1f2",
-
-          border: "rgb(0 ,0, 0.38)",
-        },
+    extend: {
+      colors: {
+        primary: "#1d9bf0",
+        hover: "rgba(0,0,0, 0.03)",
+        hoverDark: "rgba(255,255,255, 0.03)",
+        border: "rgb(0 ,0, 0.38)",
       },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
