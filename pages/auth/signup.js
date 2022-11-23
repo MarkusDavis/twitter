cc9eedd77bdc1ffce489c583dad29f451d210f50
Markus Davis <business@markusdavis.com>
2022-11-23 13:20:56 -0500

    Create signup.js

---
+++ pages/auth/signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function signup() {
+  return (
+    <div className="">
+      <div class="flex justify-between">
+        <div class="item w-32 h-32">1</div>
+        <div class="item w-32 h-32">2</div>
+      </div>
+    </div>
+  );
+}
+
+export default signup;
